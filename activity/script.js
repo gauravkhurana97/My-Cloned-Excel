6f505165b397ed66024c26ad03879ffa1ab8f0c7
gauravkhurana97 <gauravkhurana97>
2020-05-16 02:38:20 +0530

    Event bubbling in all cell

--- activity/script.js
+++ activity/script.js
@@ -1,3 +1,23 @@
 const $ = require("jquery");
 const fs = require("fs");
-const dialog = require("electron").remote.dialog;   
+const dialog = require("electron").remote.dialog;
+$(document).ready(
+    function () {
+
+        $("#grid .cell").on("click", function () {
+            // let cCell=this
+            let ri = Number($(this).attr("ri"));
+            let ci = Number($(this).attr("ci"));
+            let Address = String.fromCharCode(65 + ci) + (ri + 1);
+            let cellObject = getCellObject(ri, ci);
+            $("#address-input").val(Address);
+            $("#formula-input").val(cellObject.formula);
+        })
+
+        function init() {
+            $("#New").trigger("click");
+        }
+        init()
+
+    }
+)
