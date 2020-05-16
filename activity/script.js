d9d48bb32c09fe02ae165d5c0f7031fa90552731
gauravkhurana97 <gauravkhurana97>
2020-05-16 20:39:32 +0530

    Added new button functionality

--- activity/script.js
+++ activity/script.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const dialog = require("electron").remote.dialog;
 $(document).ready(
     function () {
-
+        let db;
         $("#grid .cell").on("click", function () {
             // let cCell=this
             let ri = Number($(this).attr("ri"));
@@ -12,6 +12,30 @@ $(document).ready(
             let cellObject = getCellObject(ri, ci);
             $("#address-input").val(Address);
             $("#formula-input").val(cellObject.formula);
+        })
+
+        // New click=> Ui and DB 
+        $("#New").on("click", function () {
+            db = [];
+            let rows = $("#grid").find(".row");
+            for (let i = 0; i < rows.length; i++) {
+                let cells = $(rows[i]).find(".cell");
+                let row = [];
+                for (let j = 0; j < cells.length; j++) {
+                    $(cells[j]).html("");
+
+                    let cell = {
+                        value: "",
+                        formula: "",
+                        downstream: [],
+                        upstream: []
+                    }
+                    row.push(cell);
+                }
+                db.push(row);
+            }
+            console.log(db);
+            $($("#grid .cell")[0]).trigger("click");
         })
 
         function init() {
